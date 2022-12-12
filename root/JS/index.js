b8cc3c5a1c30bbd3dc8696938100c5ed74d8ff4f
Naman Biyani <99156947+NamanBiyani06@users.noreply.github.com>
2022-12-12 10:08:45 -0500

    Update index.js

--- root/JS/index.js
+++ root/JS/index.js
@@ -70,6 +70,97 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
+(() => {
+  const genSvg = (target) => {
+    target.insertAdjacentHTML(
+      "afterbegin", 
+      `<svg id="svg-spinner" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none" xmlns="http://www.w3.org/2000/svg"></svg>`
+    );
+    return target.firstChild;
+  }
+  
+  const genRects = (svg, colors) => {
+    colors.forEach((color, i) => {
+      svg.insertAdjacentHTML(
+        "beforeend", 
+        `<rect x="2.5" y="2.5" fill="none" stroke-width="5" rx="5"/>`
+      );
+      svg.lastChild.style.stroke = "#" + colors[i] + "55";
+      svg.lastChild.style.willChange = "stroke-dashoffset, stroke-dasharray";
+    });
+  }
+  const setRectAttributes = (svg) => {
+    const wh = {
+      w: svg.clientWidth,
+      h:  svg.clientHeight,
+    };
+    for (let child of svg.children) {
+      child.style.width = wh.w - 5;
+      child.style.height = wh.h - 5;
+    }
+  }
+  
+  const genKeyFrames = (css, i, len) => {
+      css.insertRule(`
+  @keyframes dash_${i} {
+    from {
+        stroke-dashoffset: ${(i + 1) * len};
+  
+    }
+    to {
+        stroke-dashoffset: ${i * len};
+  
+    }
+  }
+  `, css.cssRules.length)
+  }
+  
+  const delAllKeyFrames = (css, len) => {
+    for (let i = 0; i < len; i++) {
+    console.log(css.cssRules[css.cssRules.length - 1])
+      css.deleteRule(css.cssRules.length - 1);
+    }
+  }
+  
+  
+  const rotateColors = (css, svg, delay, direction) => {
+    const len = svg.children[0].getTotalLength() / svg.children.length;
+    for (let i = 0; i < svg.children.length; i++) {
+      genKeyFrames(css, i, len);
+      const child = svg.children[i];
+      // reset for resonsiveness/animation
+      child.style.strokeDasharray = `${len}, ${len * (svg.children.length - 1)}`;
+      child.style.animation = `dash_${i} ${delay * 0.001}s linear ${direction === "reverse" ? "reverse" : ""} infinite`;
+    }
+    const child = direction === "reverse" ? svg.lastChild : svg.firstChild;
+    svg.removeChild(child);
+    direction === "reverse" ? svg.insertBefore(child, svg.firstChild) : svg.appendChild(child);
+  }
+  
+  const initiate = () => {
+    const colors = ["DB4437", "0F9D58", "F4B400", "4285F4"];
+    const target = document.querySelector(".xyz");
+    const css = document.styleSheets[document.styleSheets.length - 1];
+    const delay = 1000;
+    const direction = "forward";
+    const svg = genSvg(target);
+    let keyFramesExist = false;
+    genRects(svg, colors);
+    
+    const main = () => {
+      keyFramesExist && delAllKeyFrames(css, svg.children.length);
+      rotateColors(css, svg, delay, direction);
+      keyFramesExist = true;
+    }
+    setRectAttributes(svg);
+    main();
+    setInterval(main, delay);
+    setInterval(setRectAttributes, Math.max(delay * 0.1, 300), svg);
+  }
+  initiate();
+  })();
+  
+
 /* CUSTOM CURSOR 
 
 class GlitchEffect {
